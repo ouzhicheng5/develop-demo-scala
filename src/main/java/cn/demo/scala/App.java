1160fbc556538848877af8086a65683f58caa927
ouzhicheng <ouzhicheng5@163.com>
2017-12-05 11:08:34 +0800

    第一次提交

---
+++ src/main/java/cn/demo/scala/App.java
@@ -0,0 +1,13 @@
+package cn.demo.scala;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
