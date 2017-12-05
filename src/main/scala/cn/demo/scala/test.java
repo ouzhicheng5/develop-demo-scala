e6787473ee161d8465d64ab6b94d7ac0406f0272
ouzhicheng <ouzhicheng5@163.com>
2017-12-05 11:58:59 +0800

    第二次提交

---
+++ src/main/scala/cn/demo/scala/test.java
@@ -0,0 +1,12 @@
+package cn.demo.scala;
+
+/**
+ * Hello world!
+ *
+ */
+public class test {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
